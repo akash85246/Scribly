7aa5ea941a722ac4aad5f160ae1616b50b7728cf
akash85246 <akash22164033@akgec.ac.in>
2025-02-15 14:51:49 +0530

    Refactor Login component with Material UI enhancements

--- src/components/common/Login.jsx
+++ src/components/common/Login.jsx
@@ -1,3 +1,41 @@
+import { Button, Container, Paper, Typography } from "@mui/material";
+import { Google } from "@mui/icons-material";
+import login from "../../assets/login.svg";
+
+import "../../styles/Login.css";
+
 export default function Login() {
-  return <>login</>;
+  function handleGoogleLogin() {
+    //using navbar google button to login
+    const google=document.getElementsByClassName("google-signin")[0];
+    google.click();
+  }
+  return (
+    <Container
+      maxWidth="xs"
+      className="container"
+    >
+      <Paper elevation={3} sx={{ p: 4, textAlign: "center", borderRadius: 3 }}>
+      <Typography className="phrase1">
+       Capture your thoughts instantly, organize effortlessly, and never let an idea slip away. Your digital sticky notes, reimagined!
+        </Typography>
+        <img src={login} alt="login" width={350} />
+        <Typography className="phrase2">
+          Welcome Back!
+        </Typography>
+        <Button
+          variant="contained"
+          startIcon={<Google />}
+          sx={{
+            backgroundColor: "#DB4437",
+            color: "white",
+            "&:hover": { backgroundColor: "#C1351D" },
+          }}
+          onClick={handleGoogleLogin}
+        >
+          Sign in with Google
+        </Button>
+      </Paper>
+    </Container>
+  );
 }
